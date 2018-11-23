23db7499834d2af81826ed5b4e6a189344ba6dd1
shaoshuai <shaoshuai@megvii.com>
2018-11-23 18:51:25 +0800

    add probe

--- footer.js
+++ footer.js
@@ -3,3 +3,12 @@ document.write("\
   <p class=\"m-0 text-center text-white\">Copyright &copy; <a href=\"https://www.megvii.com\">Megvii Inc.</a> 2018</p>\
 </div>\
 ");
+
+// probe of tongji.baidu.com
+var _hmt = _hmt || [];
+(function() {
+      var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?5bc16cbe42eed99c945a9f2780d9f181";
+          var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+})();
